f8eb8a137ad97878830602b020489fdfe57882e8
Xanthus <Xanthus58@protonmail.com>
2022-10-24 19:15:05 +1030

    Fix: Menu to be a function
    Allows us to call it back later if the user wishes to play  more then 1 game

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@ use colour::*;
 use std::io;
 
 // A function that gets user input and returns it as a i32
-fn get_number() -> i32 {
+fn menu() -> i32 {
+    // Menu Options
+    println!("Select an option from below:");
+    println!("[1] Start");
+    println!("[2] Unfair Mode");
+    println!("[3] Quit");
+
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
@@ -11,15 +17,7 @@ fn get_number() -> i32 {
 }
 
 fn main() {
-    println!("Hello, world!");
-
-    // Menu Options
-    println!("Select an option from below:");
-    println!("[1] Start");
-    println!("[2] Unfair Mode");
-    println!("[3] Quit");
-
     // Menu Input
-    let x = get_number();
+    let x = menu();
     println!("You selected {}", x);
 }
