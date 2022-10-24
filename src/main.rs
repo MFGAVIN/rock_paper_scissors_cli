62d4365c5db2bbdf725d55b7898ec516e8c9c495
Matthew Pearson <mfgavin@proton.me>
2022-10-24 04:35:00 -0400

    Add: menu options and input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+    // Menu Options
+    println!("Select an option from below:");
+    println!("[1] Start");
+    println!("[2] Unfair Mode");
+    println!("[3] Quit");
+
+    // Menu Input
+    let mut number = String::new();
+    io::stdin()
+        .read_line(&mut number)
+        // Error Handler
+        .expect("Failed to read input");
 }
