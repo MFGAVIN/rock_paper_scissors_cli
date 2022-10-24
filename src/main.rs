8b14d4821133667933d6524838c88cb2ddfc4d2b
Matthew Pearson <mfgavin@proton.me>
2022-10-24 04:49:25 -0400

    Add: functions for menu options

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use colour::*;
-use std::io;
+use std::{io, process::exit};
 
 // A function that gets user input and returns it as a i32
 fn menu() -> i32 {
@@ -16,8 +16,26 @@ fn menu() -> i32 {
     input.trim().parse().expect("Failed to parse input")
 }
 
+fn start_game() {
+
+}
+
+fn unfair_mode() {
+    
+}
+
+
 fn main() {
     // Menu Input
     let x = menu();
     println!("You selected {}", x);
+
+    // If statements to sort number and call function
+    if x == 1 {
+        start_game();
+    } else if x == 2 {
+        unfair_mode();
+    } else if x == 3 {
+        exit;
+    }
 }
